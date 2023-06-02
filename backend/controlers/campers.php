2766483d280f05c059390e708e13f4c70b09dfe2
julian-vega-blanco <julianvegadragon@gmail.com>
2023-06-02 12:55:09 -0500

    cositas

--- backend/controlers/campers.php
+++ backend/controlers/campers.php
@@ -28,9 +28,18 @@ require_once ("../models/Camper.php");
         echo json_encode($datos);
          
         break;
+
+        case "insert":
+
+            $datos=$camper->dba_insert($body["id"],$body["image"],$body["nombre"],$body["edad"],$body["promedio"],$body["nivelCampus"],$body["nivelIngles"],
+            $body["especialidad"],$body["direccion"],$body["cedular"],$body["Ingles"],$body["Ser"],$body["Review"],$body["Skills"],$body["Assitencia"]);
+            echo json_encode("insertado correctamente");
+    
+            break;
         
     }
 
+    
 
 
 ?>
--- backend/models/Camper.php
+++ backend/models/Camper.php
@@ -30,6 +30,32 @@ try {
 
 }
 
+
+public function insert_camper($id,$imagen,$nombre,$edad, $promedio, $nivelCAmpus, $nivelIngles, $especialidad, $direccion, $celular, $Ingles, $Ser,$Review, $Skills, $Asitencia ){
+    $conectar=parent::conexion();
+    parent::set_name();
+    $stm="INSERT INTO campers(id,imagen,nombre,edad,promedio,nivelCAmpus,nivelIngles, especialidad, direccion, celular, Ingles, Ser, Review, Skills, Asitencia) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    $stm=$conectar->prepare($stm);
+    $stm->bindValue(1,$id);
+    $stm->bindValue(2,$imagen);
+    $stm->bindValue(3,$nombre);
+    $stm->bindValue(4,$edad);
+    $stm->bindValue(5,$promedio);
+    $stm->bindValue(6,$nivelCAmpus);
+    $stm->bindValue(7,$nivelIngles);
+    $stm->bindValue(8,$especialidad);
+    $stm->bindValue(9,$direccion);
+    $stm->bindValue(10,$celular);
+    $stm->bindValue(11,$Ingles);
+    $stm->bindValue(12,$Ser);
+    $stm->bindValue(13,$Review);
+    $stm->bindValue(14,$Skills);
+    $stm->bindValue(15,$Asitencia);
+    $stm->execute();
+    return $stm->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
 }
 
 ?>
